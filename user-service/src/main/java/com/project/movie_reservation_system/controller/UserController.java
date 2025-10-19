ea2d7ec7492b97d5cbdab3f1ed02493ae4937220
mquang279 <quangm279@gmail.com>
2025-10-19 23:47:52 +0700

    refactor: update UserController to enhance user management endpoints

--- user-service/src/main/java/com/project/movie_reservation_system/controller/UserController.java
+++ user-service/src/main/java/com/project/movie_reservation_system/controller/UserController.java
@@ -1,10 +1,19 @@
 package com.project.movie_reservation_system.controller;
 
+import com.project.movie_reservation_system.dto.entity.UserDTO;
+import com.project.movie_reservation_system.dto.request.CreateUserRequest;
+import com.project.movie_reservation_system.dto.request.UpdateUserRequest;
 import com.project.movie_reservation_system.dto.response.PaginationResponse;
-import com.project.movie_reservation_system.dto.response.UserResponseDto;
 import com.project.movie_reservation_system.service.UserService;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -16,17 +25,35 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/me")
-    public ResponseEntity<UserResponseDto> currentUser() {
-        return ResponseEntity.ok(userService.getCurrentUser());
-
+    @PostMapping("")
+    public ResponseEntity<UserDTO> createUser(@RequestBody CreateUserRequest request) {
+        UserDTO userDTO = this.userService.createUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDTO);
     }
 
     @GetMapping("")
-    public ResponseEntity<PaginationResponse<UserResponseDto>> getAllUsers(
+    public ResponseEntity<PaginationResponse<UserDTO>> getAllUsers(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int pageSize) {
-        return ResponseEntity.ok(
-                userService.getAllUser(page, pageSize));
+            @RequestParam(defaultValue = "15") int pageSize) {
+        PaginationResponse<UserDTO> response = this.userService.getAllUser(page, pageSize);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        UserDTO userDTO = this.userService.getUserById(id);
+        return ResponseEntity.ok().body(userDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UpdateUserRequest request) {
+        UserDTO userDTO = this.userService.updateUser(id, request);
+        return ResponseEntity.ok().body(userDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        this.userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
