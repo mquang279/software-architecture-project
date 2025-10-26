14fa3cfed9fa164e8f240c4b1f12205d8e3f17e8
Mquang279 <quangm279@gmail.com>
2025-10-26 10:40:33 +0700

    chore: add gateway-service source code

--- api-gateway/src/main/java/com/project/movie_reservation_system/repository/SeatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.movie_reservation_system.repository;
-
-import com.project.movie_reservation_system.entity.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeatRepository extends JpaRepository<Seat, Long> {
-}
