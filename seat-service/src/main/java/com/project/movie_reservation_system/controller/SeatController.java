d826a551714e2648247b7017e95bd52db1ad56bd
quanpm2008 <quan2082005@gmail.com>
2025-10-27 23:35:43 +0700

    feat: add seat service

---
+++ seat-service/src/main/java/com/project/movie_reservation_system/controller/SeatController.java
@@ -0,0 +1,4 @@
+package com.project.movie_reservation_system.controller;
+
+public class ReservationController {
+}
--- seat-service/src/main/java/com/project/movie_reservation_system/exception/MovieNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.project.movie_reservation_system.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class MovieNotFoundException extends CustomException {
-    public MovieNotFoundException(String message, HttpStatus httpStatus) {
-        super(message, httpStatus);
-    }
-}
