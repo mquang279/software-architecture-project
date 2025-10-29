f7543da6e6c7793fd711f0979a5b042f05430ea8
quanpm2008 <quan2082005@gmail.com>
2025-10-29 10:01:54 +0700

    test

--- reservation-service/src/main/java/com/project/reservation_service/dto/NotificationDto.java
+++ reservation-service/src/main/java/com/project/reservation_service/dto/NotificationDto.java
@@ -1,11 +1,21 @@
 package com.project.reservation_service.dto;
 
+import com.project.reservation_service.enums.NotificationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class NotificationDto {
+    private Long id;
+    private Long userId;
+    private NotificationType type;
+    private String payload;
+    private Integer readFlag;
+    private Instant createdAt;
 }
