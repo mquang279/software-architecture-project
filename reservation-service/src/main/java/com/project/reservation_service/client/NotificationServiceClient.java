ace1e218c10ea117557a814d772d952d0d27d518
quanpm2008 <quan2082005@gmail.com>
2025-10-29 09:41:15 +0700

    feat: update notification

---
+++ reservation-service/src/main/java/com/project/reservation_service/client/NotificationServiceClient.java
@@ -0,0 +1,4 @@
+package com.project.reservation_service.client;
+
+public interface NotificationServiceClient {
+}
---
+++ reservation-service/src/main/java/com/project/reservation_service/dto/NotificationRequestDto.java
@@ -0,0 +1,4 @@
+package com.project.reservation_service.dto;
+
+public class NotificationRequestDto {
+}
