4fae106b0b1c69651ed275f0e83e358fc23b256a
Mquang279 <quangm279@gmail.com>
2025-11-05 14:24:20 +0700

    feat: modifying endpoint path in TheaterController

--- theater-service/src/main/java/com/project/movie_reservation_system/controller/TheaterController.java
+++ theater-service/src/main/java/com/project/movie_reservation_system/controller/TheaterController.java
@@ -1,12 +1,10 @@
 package com.project.movie_reservation_system.controller;
 
-import com.project.movie_reservation_system.dto.ApiResponse;
 import com.project.movie_reservation_system.dto.PaginationResponse;
 import com.project.movie_reservation_system.dto.TheaterRequestDto;
 import com.project.movie_reservation_system.entity.Theater;
 import com.project.movie_reservation_system.service.impl.TheaterServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,31 +23,28 @@ public class TheaterController {
     @GetMapping("")
     public ResponseEntity<PaginationResponse<Theater>> getAllTheaters(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
+            @RequestParam(defaultValue = "10") int size) {
         PaginationResponse<Theater> response = theaterService.getAllTheaters(page, size);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/locations")
+    @GetMapping("")
     public ResponseEntity<PaginationResponse<Theater>> getAllTheatersByLocation(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
-            @RequestParam String location
-    ){
+            @RequestParam String location) {
         PaginationResponse<Theater> response = theaterService.getAllTheatersByLocation(page, size, location);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{theaterId}")
-    public ResponseEntity<Theater> getTheaterById(@PathVariable long theaterId){
+    public ResponseEntity<Theater> getTheaterById(@PathVariable long theaterId) {
         Theater theater = theaterService.getTheaterById(theaterId);
         return ResponseEntity.ok(theater);
     }
 
-
     @PostMapping("")
-    public ResponseEntity<Theater> createTheater(@RequestBody TheaterRequestDto theaterRequestDto){
+    public ResponseEntity<Theater> createTheater(@RequestBody TheaterRequestDto theaterRequestDto) {
         Theater theater = theaterService.createNewTheater(theaterRequestDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -58,19 +53,18 @@ public class TheaterController {
     }
 
     @PutMapping("{theaterId}")
-    public ResponseEntity<Theater> updateTheaterById(@PathVariable long theaterId, @RequestBody TheaterRequestDto theaterRequestDto){
+    public ResponseEntity<Theater> updateTheaterById(@PathVariable long theaterId,
+            @RequestBody TheaterRequestDto theaterRequestDto) {
         Theater updatedTheater = theaterService.updateTheaterById(theaterId, theaterRequestDto);
         return ResponseEntity.ok(updatedTheater);
     }
 
-
     @DeleteMapping("{theaterId}")
-    public ResponseEntity<?> deleteTheaterById(@PathVariable long theaterId){
+    public ResponseEntity<?> deleteTheaterById(@PathVariable long theaterId) {
         theaterService.deleteTheaterById(theaterId);
         return ResponseEntity
                 .status(HttpStatus.NO_CONTENT)
                 .build();
     }
-
 
 }
